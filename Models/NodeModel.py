78bddf4badf8a7b3ff1bf82ab4d3018cffe58c46
chieryw <chieryw@163.com>
2019-03-15 15:28:09 +0800

    更改数据类型

--- Models/NodeModel.py
+++ Models/NodeModel.py
@@ -1,13 +1,11 @@
-from sqlalchemy.dialects import postgresql
-from sqlalchemy import FLOAT
 from app import db
 
 class NodeModel(db.Model):
     __tablename__ = 'nodes'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), index=True, unique=True)
-    subtitle = db.Column(db.String(300), index=True, unique=True)
-    image = db.Column(db.String(100), index=True, unique=True)
-    transform = db.Column(postgresql.ARRAY(FLOAT))
-    type = db.Column(db.Integer, index=True, unique=True)
+    title = db.Column(db.String(200))
+    subtitle = db.Column(db.String(300))
+    image = db.Column(db.String(100))
+    transform = db.Column(db.String(500))
+    nodetype = db.Column(db.Integer)
 
